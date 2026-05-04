7e67442b7718416057dd03f79cb2f34000ca0b3f
sanugaLyx <sanugaliyanage12@gmail.com>
2026-05-05 01:28:37 +0530

    Initial_Files

---
+++ Backend/SCRS/src/test/java/com/scrs/ScrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
